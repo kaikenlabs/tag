d2227349ab13b3f2a89415a9c908c75e9cf73058
Juan <179163895+th3godfather@users.noreply.github.com>
2026-02-09 18:59:08 +0100

    Add labels to linters

--- scripts/lints.mk
+++ scripts/lints.mk
@@ -3,9 +3,13 @@
 #####################
 
 lint: ## Lint tools
-	go vet ./...
+	@echo "Running go vet"
+	@go vet ./...
+	@echo "Running gofumpt"
 	@${GOBIN}/gofumpt -l -w .
+	@echo "Running deadcode"
 	@${GOBIN}/deadcode ./...
+	@echo "Running golangci-lint"
 	@${GOBIN}/golangci-lint -c .golangci.yaml run ./...
 
 scan: ## Security scanning
